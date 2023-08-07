70c82b54b7630af736104b370d64ec47710c0986
Olesia Andreiko <olesikmail@gmail.com>
2023-08-07 22:44:12 +0300

    task done

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,13 @@
 import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
+flsFunctions.cssAnim();
+
+
 
 import Swiper, { Navigation, Pagination } from 'swiper';
 
 const swiper = new Swiper();
+
+
+
--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -19,3 +19,29 @@ export function isWebp() {
     //   }
   });
 }
+
+export function cssAnim() {
+  let options = {
+    root:null,
+    rootMargin:"5px",
+    threshold: 0.5
+}
+
+let callback = function (entries, observer) {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            // console.log("find", entry)
+            entry.target.classList.add("active");
+            observer.unobserve(entry.target);
+        }
+    });
+
+}
+
+let observer = new IntersectionObserver(callback, options);
+
+let targets = document.querySelectorAll(".anim")
+targets.forEach(target => {
+    observer.observe(target)
+})
+}
